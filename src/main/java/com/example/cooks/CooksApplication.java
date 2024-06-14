eb6541c446b7c6b67b592e11b76c14e55d3ae9ac
Jyaheza Irakoze Maurice <mauriceirakoze77@gmail.com>
2024-06-14 11:09:19 -0500

    initial commit

---
+++ src/main/java/com/example/cooks/CooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.cooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooksApplication.class, args);
+	}
+
+}
